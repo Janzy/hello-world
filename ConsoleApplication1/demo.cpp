397c8cdab495d0ac4c54d9109fdc35d119564771
Janzy <janzy.atk@gmail.com>
2015-02-25 15:15:24 +0200

    personalisedGreeting

--- ConsoleApplication1/demo.cpp
+++ ConsoleApplication1/demo.cpp
@@ -1,6 +1,12 @@
 // Our first C++ program(hello,world)
 #include <iostream>
+#include <string>
 
 int main() {
-	std::cout << "hello, world!" << std::endl;
+
+	std::string myName;
+
+	std::cout << "please enter your name:";
+	std::cin >> myName;
+	std::cout << "hello," << myName << std::endl;
 }
